0d33e2a19c7c6843e082ea62ac468c0e524bbcfe
Verity <83372423+VerityIncorporated@users.noreply.github.com>
2024-09-07 16:09:18 +0100

    MainMenu hook
    Added main menu hook which disables the prefab when you are not in-game.

---
+++ TooManySuits/Hooks/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+
+namespace TooManySuits.Hooks;
+
+[HarmonyPatch(typeof(MenuManager), "Start")]
+public class MainMenu
+{
+    [HarmonyPostfix]
+    public static void MainMenuHook(MenuManager __instance)
+    {
+        var prefab = TooManySuits.SuitRackPrefab;
+        if (__instance.isInitScene || !prefab.activeSelf) return;
+
+        prefab.SetActive(false);
+    }
+}
